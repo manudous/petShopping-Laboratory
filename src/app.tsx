c40ca6f1d3c88201fabfc20a8bf22ab7f89d0615
Manudous <manudous@gmail.com>
2021-01-29 16:32:28 +0100

    add MyContextProvider

--- src/app.tsx
+++ src/app.tsx
@@ -6,10 +6,9 @@ import { RoutesComponent } from "./routes/RoutesComponent";
 export const App = () => {
   return (
     <>
-    <MyContextProvider>
-      
-    </MyContextProvider>
-      <RoutesComponent />
+      <MyContextProvider>
+        <RoutesComponent />
+      </MyContextProvider>
     </>
   );
 };
